9074371b93ca40ecc1f2e311db906a64b41b9291
Mathias Elken <math079e@edu.eal.dk>
2018-10-03 13:50:07 +0200

    Sum

--- CalculatorLibrary/Calculator.cs
+++ CalculatorLibrary/Calculator.cs
@@ -60,5 +60,15 @@ namespace CalculatorLibrary
             return resultat;
         }
 
+        public static double Sum(double[] numbers)
+        {
+            double sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+        }
     }
 }
